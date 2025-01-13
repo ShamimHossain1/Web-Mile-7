0bec115e5c4be461b8bb9f94c02e4fb61e3207ae
ShamimHossain1 <shamimweb78@gmail.com>
2025-01-12 22:57:55 -0500

    conditional css

--- react-world-tour/src/components/Countires/Countries.jsx
+++ react-world-tour/src/components/Countires/Countries.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import Country from "../Country/Country";
+import './Countries.css'
 
 const Countries = () => {
     const [countries, setCountries] = useState([]);
@@ -8,13 +9,16 @@ const Countries = () => {
         .then(response => response.json())
         .then(data => setCountries(data))
     },[]);
-    console.log(countries);
+    // console.log(countries);
     return (
         <div>
-            <h3>Countries: {countries.length}</h3>
+        <h3>Countries: {countries.length}</h3>
+        <div className="country-container">
+           
             {
                 countries.map((country) => <Country country={country} key={country.cca3}></Country>)
             }
+        </div>
         </div>
     );
 };
--- react-world-tour/src/components/Country/Country.jsx
+++ react-world-tour/src/components/Country/Country.jsx
@@ -1,12 +1,24 @@
+import { useState } from 'react';
 import './Country.css'
 const Country = ({ country }) => {
 
-    const { name, flags } = country;
+    const { name, flags,population,area, cca3 } = country;
+    const [Visited, setVisited] = useState(false);
+
+    function handleVisited(){
+        setVisited(!Visited);
+    }
 
     return (
-        <div className="country">
+        <div  className={`country ${Visited? 'visited' : 'not-visited'}`}>
             <h3>Name: {name?.common}</h3>
             <img src={flags.png} alt="flag" />
+            <p>Population: {population}</p>
+            <p>Area: {area}</p>
+            <p><small>Code: {cca3}</small></p>
+            <button onClick={handleVisited}>{Visited ? 'Visited' : 'Going'}</button>
+            {Visited &&" I have visited This country"}
+
         </div>
     );
 };
