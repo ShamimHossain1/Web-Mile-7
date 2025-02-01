1ea630838bf64515b80ab6ded55c3318990fbdb3
ShamimHossain1 <shamimweb78@gmail.com>
2025-01-31 23:55:14 -0500

    blogs layout

--- Knowledge-cafe/src/Components/Blogs/Blog/Blog.jsx
+++ Knowledge-cafe/src/Components/Blogs/Blog/Blog.jsx
@@ -1,31 +1,40 @@
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBookmark } from '@fortawesome/free-solid-svg-icons';
 import PropTypes from 'prop-types';
 const Blog = ({blog}) => {
-    const {author, author_img, cover, hastags, id, posted_date, reading_time, title} = blog;
-    console.log(posted_date);
+    const {author, author_img, cover, hashtags, posted_date, reading_time, title} = blog;
+    // console.log(posted_date);
     return (
         <div>
               <div className="mt-4">
                 <img src={cover} alt={title} className="w-full h-64 object-cover rounded-lg" />
             </div>
-            <div className="flex items-center justify-between">
+            <div className="flex items-center justify-between mt-5">
                 <div className="flex items-center">
                     <img src={author_img} alt={author} className="w-10 h-10 rounded-full" />
                     <div className="ml-3">
-                        <h3 className="text-sm font-medium text-gray-900">{author}</h3>
+                        <h3 className="text-xl font-bold text-gray-900">{author}</h3>
                         <p className="text-sm font-light text-gray-500">{posted_date}</p>
                     </div>
                 </div>
                 <div className='flex items-center gap-3'>
                 <p className="text-sm font-light text-gray-500">{reading_time}</p>
-                    <button className="text-sm font-medium text-blue-500 hover:text-blue-700">Save</button>
+                <button><FontAwesomeIcon icon={faBookmark} /></button>
                 </div>
             </div>
           
             <div className="mt-4">
-                <h2 className="text-2xl font-semibold text-gray-900">{title}</h2>
-                <p className="mt-2 text-sm font-light text-gray-500">{hastags}</p>
+                <h2 className="text-2xl font-semibold text-gray-900 mb-4">{title}</h2>
+                {
+                    hashtags.map((hastag, idx) => (
+                        <a key={idx} className="mt-2 text-sm font-light text-gray-500"> #{hastag}</a>
+                    ))
+                } <br />
+                <button className='text-blue-600 underline font-semibold'>Mark as read</button>
+                
             </div>
+            <hr  className='text-gray-300 my-10'/>
         </div>
     );
 };
